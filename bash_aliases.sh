abc62e36f6898e47133ba30c648c3a975c9aaa8b
Hayaka Kayori <fisevcik@gmail.com>
2022-07-05 00:55:23 +0200

    Aliases

---
+++ bash_aliases.sh
@@ -0,0 +1,25 @@
+#linux workflow aliases by Hayaka Kayori @ 2021
+# protect cp, mv, rm command with confirmation + verbose
+alias cp='cp -iv'
+alias mv='mv -iv'
+alias rm='rm -iv'
+#make directory inside directory
+alias mkd='mkdir -pv'
+
+#ls aliases
+
+alias ls='ls -F --color=auto' 
+alias la='ls -Ah' 
+alias ll='ls -lah' 
+alias lsa='ls -lasSh'
+alias lt='ls -lahS'
+alias l='ls -C'
+
+#myip from ipinfo.io
+alias myip='curl ipinfo.io/ip'
+
+#tar
+alias untar='tar -zxvf'
+
+
+
